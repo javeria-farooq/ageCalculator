3fe400e5c512f6c912559793b317237177381988
javeria-farooq <gettingeducation6@gmail.com>
2024-05-28 02:08:45 +0500

    Include style sheet

--- app.js
+++ app.js
@@ -1,19 +1,53 @@
-let pro = prompt('Enter your date of birth')
+/*me */
+
+let input = document.querySelector(".input")
+let btn = document.querySelector(".calculate-btn")
+let userAge = document.querySelector(".user-age-text")
 
 let date = new Date()
-let dateMili = date.getTime()
+// document.write(date)
+let myDate = date.getTime()
+document.write(myDate)
 
-let dob = new Date (pro)
-let dobMili = dob.getTime()
 
-let minus = dateMili - dobMili
-let formula = minus / (1000 * 60 * 60 * 24 * 365)
-let age = Math.floor(formula)
+function calcAge() {
 
-if (age >= 0) {
-    alert('your age is : ' + age)
 }
 
-else {
-    alert('Invalid age')
-}
+// btn.addEventListener("click", )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*sr */
+
+// let pro = prompt('Enter your date of birth')
+
+// let date = new Date()
+// let dateMili = date.getTime()
+
+// let dob = new Date (pro)
+// let dobMili = dob.getTime()
+
+// let minus = dateMili - dobMili
+// let formula = minus / (1000 * 60 * 60 * 24 * 365)
+// let age = Math.floor(formula)
+
+// if (age >= 0) {
+//     alert('your age is : ' + age)
+// }
+
+// else {
+//     alert('Invalid age')
+// }
